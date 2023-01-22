207f72527c12257788a8fd3cf4015c679b4a63a3
Nick Gastis <nickgastis1@gmail.com>
2023-01-22 16:39:04 -0500

    All done

---
+++ index.js
@@ -0,0 +1,17 @@
+const name = Aki
+
+function introduction(name) {
+    return (`Hi, my name is ${name}.`)
+}
+
+
+
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+
+}
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
